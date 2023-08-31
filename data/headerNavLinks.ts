55cc9552ad29be1380388ec052af15fb9d29a4ea
Edi <82562025+edisaso@users.noreply.github.com>
2023-08-31 19:58:17 +0300

    Add files via upload

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/News', title: 'News' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
