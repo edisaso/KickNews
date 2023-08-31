927ed8535d898453d6d8947629a9f0b470c184ae
Edi <82562025+edisaso@users.noreply.github.com>
2023-08-31 19:59:27 +0300

    Add files via upload

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+import { allNews } from 'contentlayer/generated'
+import siteMetadata from '@/data/siteMetadata'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMetadata.siteUrl
+  const NewsRoutes = allNews.map((post) => ({
+    url: `${siteUrl}/${post.path}`,
+    lastModified: post.lastmod || post.date,
+  }))
+
+  const routes = ['', 'News', 'projects', 'tags'].map((route) => ({
+    url: `${siteUrl}/${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes, ...NewsRoutes]
+}
