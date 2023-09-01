580c9d0f774d9eb047bbe10ff62087f8c4f0e632
Edi <82562025+edisaso@users.noreply.github.com>
2023-09-01 23:45:23 +0300

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   //description: 'Website created to cover Kick.com news',
   language: 'en-us',
   theme: 'dark', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://kicknews.vercel.app',
   siteRepo: 'https://github.com/edisaso/KickNews',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
