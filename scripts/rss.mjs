6177911a3498ea0330bc231e474c1317bdf96608
Edi <82562025+edisaso@users.noreply.github.com>
2023-09-01 16:49:06 +0300

    Update rss.mjs

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,7 +1,6 @@
 import { writeFileSync, mkdirSync } from 'fs'
 import path from 'path'
 import GithubSlugger from 'github-slugger'
-import { escape } from 'pliny/utils/htmlEscaper.js'
 import siteMetadata from '../data/siteMetadata.js'
 import tagData from '../app/tag-data.json' assert { type: 'json' }
 import { allNews } from '../.contentlayer/generated/index.mjs'
@@ -13,9 +12,7 @@ const generateRssItem = (config, post) => {
   return `
     <item>
       <guid>${config.siteUrl}/News/${post.slug}</guid>
-      <title>${escape(title)}</title>
       <link>${config.siteUrl}/News/${post.slug}</link>
-      ${summary && `<description>${escape(summary)}</description>`}
       <pubDate>${new Date(post.date).toUTCString()}</pubDate>
       <author>${config.email} (${config.author})</author>
       ${post.tags && post.tags.map((t) => `<category>${t}</category>`).join('')}
@@ -27,9 +24,7 @@ const generateRssItem = (config, post) => {
 const generateRss = (config, posts, page = 'feed.xml') => `
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
-      <title>${escape(config.title)}</title>
       <link>${config.siteUrl}/News</link>
-      <description>${escape(config.description)}</description>
       <language>${config.language}</language>
       <managingEditor>${config.email} (${config.author})</managingEditor>
       <webMaster>${config.email} (${config.author})</webMaster>
