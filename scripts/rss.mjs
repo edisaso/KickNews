d6e47e27df437c9d1d08eae3fb387bfada55ab44
Edi <82562025+edisaso@users.noreply.github.com>
2023-09-01 16:39:39 +0300

    Update rss.mjs

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,30 +1,62 @@
-// scripts/rss.mjs
-import fs from 'fs';
-import matter from 'gray-matter';
-import path from 'path';
+import { writeFileSync, mkdirSync } from 'fs'
+import path from 'path'
+import GithubSlugger from 'github-slugger'
+import { escape } from 'pliny/utils/htmlEscaper.js'
+import siteMetadata from '../data/siteMetadata.js'
+import tagData from '../app/tag-data.json' assert { type: 'json' }
+import { allNews } from '../.contentlayer/generated/index.mjs'
 
-const generateRss = async () => {
-  const POSTS_DIR = path.join(process.cwd(), 'content');
-  const posts = fs
-    .readdirSync(POSTS_DIR)
-    .filter((filePath) => filePath !== '.DS_Store') // only needed for macOS
-    .map((filePath) => {
-      const source = fs.readFileSync(path.join(POSTS_DIR, filePath));
-      const { content, data } = matter(source);
+const generateRssItem = (config, post) => `
+  <item>
+    <guid>${config.siteUrl}/News/${post.slug}</guid>
+    <title>${escape(post.title)}</title>
+    <link>${config.siteUrl}/News/${post.slug}</link>
+    ${post.summary && `<description>${escape(post.summary)}</description>`}
+    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    <author>${config.email} (${config.author})</author>
+    ${post.tags && post.tags.map((t) => `<category>${t}</category>`).join('')}
+  </item>
+`
 
-      return {
-        content,
-        data,
-        filePath,
-      };
-    })
-    .sort(
-      (a, b) =>
-        new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
-    );
+const generateRss = (config, posts, page = 'feed.xml') => `
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+      <title>${escape(config.title)}</title>
+      <link>${config.siteUrl}/News</link>
+      <description>${escape(config.description)}</description>
+      <language>${config.language}</language>
+      <managingEditor>${config.email} (${config.author})</managingEditor>
+      <webMaster>${config.email} (${config.author})</webMaster>
+      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <atom:link href="${config.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
+      ${posts.map((post) => generateRssItem(config, post)).join('')}
+    </channel>
+  </rss>
+`
 
-  // Create sitemap file
-  fs.writeFileSync('src/pages/api/rss/feed.json', JSON.stringify(posts));
-};
+async function generateRSS(config, allNews, page = 'feed.xml') {
+  const publishPosts = allNews.filter((post) => post.draft !== true)
+  // RSS for News post
+  if (publishPosts.length > 0) {
+    const rss = generateRss(config, publishPosts)
+    writeFileSync(`./public/${page}`, rss)
+  }
 
-generateRss();
+  if (publishPosts.length > 0) {
+    for (const tag of Object.keys(tagData)) {
+      const filteredPosts = allNews.filter((post) =>
+        post.tags.map((t) => GithubSlugger.slug(t)).includes(tag)
+      )
+      const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
+      const rssPath = path.join('public', 'tags', tag)
+      mkdirSync(rssPath, { recursive: true })
+      writeFileSync(path.join(rssPath, page), rss)
+    }
+  }
+}
+
+const rss = () => {
+  generateRSS(siteMetadata, allNews)
+  console.log('RSS feed generated...')
+}
+export default rss
